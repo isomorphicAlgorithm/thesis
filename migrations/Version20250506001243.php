5da8a2e0cdca67c19ff530ea906b4d7ea8316c16
FILIP <dark-shadow-95@windowslive.com>
2025-05-06 03:13:29 +0300

    Updated previous commit

--- migrations/Version20250506001243.php
+++ migrations/Version20250506001243.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20250504030322 extends AbstractMigration
+final class Version20250506001243 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,16 +20,12 @@ final class Version20250504030322 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql(<<<'SQL'
-            ALTER TABLE user ADD roles JSON NOT NULL, DROP is_admin
-        SQL);
+
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql(<<<'SQL'
-            ALTER TABLE `user` ADD is_admin TINYINT(1) NOT NULL, DROP roles
-        SQL);
+
     }
 }
