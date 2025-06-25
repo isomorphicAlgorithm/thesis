c59aabba9277e798d3935f43ba096f8d306d1c0c
FILIP <dark-shadow-95@windowslive.com>
2025-06-25 22:50:41 +0300

    Remove existing app folder to prepare subtree add

--- app/README.md
+++
@@ -1,6 +0,0 @@
-# web-thesis
-Bandito Project - Web Thesis
-
-cd docker
-cp .env.example .env
-docker compose up --build
--- app/src/Service/MusicBrainzService.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace App\Service;
-
-use App\Service\Client\AbstractClient;
-
-class MusiBrainzService extends AbstractClient {}
